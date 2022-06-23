b6a71bae02da1e4c303c493640179105fedb5488
DaeHyeonHong <fholng@outlook.com>
2022-06-23 22:31:36 +0900

    Pointcut

--- src/test/java/hello/proxy/advisor/AdvisorTests.java
+++ src/test/java/hello/proxy/advisor/AdvisorTests.java
@@ -3,11 +3,16 @@ package hello.proxy.advisor;
 import hello.proxy.common.advice.TimeAdvice;
 import hello.proxy.common.service.ServiceImpl;
 import hello.proxy.common.service.ServiceInterface;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.MethodMatcher;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.PointcutAdvisor;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
+import java.lang.reflect.Method;
 
 class AdvisorTests {
     @Test
@@ -20,5 +25,52 @@ class AdvisorTests {
 
         proxy.save();
         proxy.find();
+    }
+
+    @Test
+    @DisplayName(value = "직접 만든 포인트컷")
+    void advisorTest2() {
+        final ServiceInterface target = new ServiceImpl();
+        final ProxyFactory proxyFactory = new ProxyFactory(target);
+        final PointcutAdvisor advisor = new DefaultPointcutAdvisor(new myPointcut(), new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+        final ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.save();
+        proxy.find();
+    }
+
+    static class myPointcut implements Pointcut {
+        @Override
+        public ClassFilter getClassFilter() {
+            return ClassFilter.TRUE;
+        }
+
+        @Override
+        public MethodMatcher getMethodMatcher() {
+            return new MyMethodMatcher();
+        }
+    }
+
+    @Slf4j
+    static class MyMethodMatcher implements MethodMatcher {
+        @Override
+        public boolean matches(final Method method, final Class<?> targetClass) {
+            String matchName = "save";
+            final boolean result = method.getName().equals(matchName);
+            log.info("Pointcut Call method={} targetClass={}", method.getName(), targetClass);
+            log.info("Pointcut Result={}", result);
+            return result;
+        }
+
+        @Override
+        public boolean isRuntime() {
+            return false;
+        }
+
+        @Override
+        public boolean matches(final Method method, final Class<?> targetClass, final Object... args) {
+            return false;
+        }
     }
 }
