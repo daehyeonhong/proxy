6cae9857365cd87f3189f602d094a1c4621df834
DaeHyeonHong <fholng@outlook.com>
2022-06-27 23:35:39 +0900

    Bean 후처리기

---
+++ src/test/java/hello/proxy/postprocessor/BasicTests.java
@@ -0,0 +1,43 @@
+package hello.proxy.postprocessor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@Slf4j
+class BasicTests {
+    @Test
+    void basicConfig() {
+        final AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(BasicConfig.class);
+        final A a = applicationContext.getBean(A.class);
+        a.helloA();
+        assertThrows(NoSuchBeanDefinitionException.class, () -> applicationContext.getBean(B.class));
+    }
+
+    @Slf4j
+    @Configuration
+    static class BasicConfig {
+        @Bean(name = "beanA")
+        public A a() {
+            return new A();
+        }
+    }
+
+    @Slf4j
+    static class A {
+        public void helloA() {
+            log.info("hello A");
+        }
+    }
+
+    @Slf4j
+    static class B {
+        public void helloB() {
+            log.info("hello B");
+        }
+    }
+}
