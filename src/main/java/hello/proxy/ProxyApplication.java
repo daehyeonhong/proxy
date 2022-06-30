05126c2e62bb2253bc873149fa406774d7ada193
DaeHyeonHong <fholng@outlook.com>
2022-06-30 22:05:41 +0900

    AOP 적용

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,6 +1,6 @@
 package hello.proxy;
 
-import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
+import hello.proxy.config.v5_autoproxy.AutoProxyConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -15,7 +15,8 @@ import org.springframework.context.annotation.Import;
 //@Import(value = {DynamicProxyFilterConfig.class})
 //@Import(value = {ProxyFactoryConfigV1.class})
 //@Import(value = {ProxyFactoryConfigV2.class})
-@Import(value = {BeanPostProcessorConfig.class})
+//@Import(value = {BeanPostProcessorConfig.class})
+@Import(value = {AutoProxyConfig.class})
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
